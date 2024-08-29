a15f7c1167a27d9177cf0c316d4ba6a774c5fe01
divya-as-1307 <asdivyashri@gmail.com>
2024-08-30 03:41:58 +0530

    updated all files

---
+++ index.js
@@ -0,0 +1,92 @@
+var images1 = document.querySelector(".images1")
+var overlay =document.querySelector(".overlay")
+var imgone = document.querySelector(".img1")
+images1.addEventListener("click",function(){
+overlay.classList.remove("hidden")
+imgone.style.display= "block"
+})
+var closex1 = document.querySelector(".closex1")
+closex1.addEventListener("click",function(){
+    overlay.classList.add("hidden")
+    imgone.style.display="none"
+})          
+// 
+var images2 = document.querySelector(".images2")
+var overlay =document.querySelector(".overlay")
+var imgtwo = document.querySelector(".img2")
+images2.addEventListener("click",function(){
+overlay.classList.remove("hidden")
+imgtwo.style.display= "block"
+})
+var closex2 = document.querySelector(".closex2")
+closex2.addEventListener("click",function(){
+    overlay.classList.add("hidden")
+    imgtwo.style.display="none"
+})
+
+// 
+var images3 = document.querySelector(".images3")
+var overlay =document.querySelector(".overlay")
+var imgthree = document.querySelector(".img3")
+images3.addEventListener("click",function(){
+overlay.classList.remove("hidden")
+imgthree.style.display= "block"
+})
+var closex3 = document.querySelector(".closex3")
+closex3.addEventListener("click",function(){
+    overlay.classList.add("hidden")
+    imgthree.style.display="none"
+})
+
+// 
+var images4 = document.querySelector(".images4")
+var overlay =document.querySelector(".overlay")
+var imgfour = document.querySelector(".img4")
+images4.addEventListener("click",function(){
+overlay.classList.remove("hidden")
+imgfour.style.display= "block"
+})
+var closex4 = document.querySelector(".closex4")
+closex4.addEventListener("click",function(){
+    overlay.classList.add("hidden")
+    imgfour.style.display="none"
+})
+// 
+var images5 = document.querySelector(".images5")
+var overlay =document.querySelector(".overlay")
+var imgfive = document.querySelector(".img5")
+images5.addEventListener("click",function(){
+overlay.classList.remove("hidden")
+imgfive.style.display= "block"
+})
+var closex5 = document.querySelector(".closex5")
+closex5.addEventListener("click",function(){
+    overlay.classList.add("hidden")
+    imgfive.style.display="none"
+})
+// 
+var images6 = document.querySelector(".images6")
+var overlay =document.querySelector(".overlay")
+var imgsix = document.querySelector(".img6")
+images6.addEventListener("click",function(){
+overlay.classList.remove("hidden")
+imgsix.style.display= "block"
+})
+var closex6 = document.querySelector(".closex6")
+closex6.addEventListener("click",function(){
+    overlay.classList.add("hidden")
+    imgsix.style.display="none"
+})
+// 
+var images7 = document.querySelector(".images7")
+var overlay =document.querySelector(".overlay")
+var imgseven = document.querySelector(".img7")
+images7.addEventListener("click",function(){
+overlay.classList.remove("hidden")
+imgseven.style.display= "block"
+})
+var closex7 = document.querySelector(".closex7")
+closex7.addEventListener("click",function(){
+    overlay.classList.add("hidden")
+    imgseven.style.display="none"
+})
